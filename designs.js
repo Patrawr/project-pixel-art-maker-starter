41901aed19ea5ae08006242285a9b3e18878de42
Patrick Wawrzoszek <pwawrzoszek@hotmail.com>
2018-02-19 22:35:49 -0500

    makeGrid() now correctly generates a grid using height and width from form.
    Also, clears out any old grid when user clicks submit button again.

--- designs.js
+++ designs.js
@@ -2,14 +2,35 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+function makeGrid(event) {
+    // prevent the page from reloading on submit
+    event.preventDefault();
+    
+    const gridHeight = document.querySelector('#inputHeight');
+    const gridWidth = document.querySelector('#inputWidth');
+
+    //store the parent canvas table element in a var
+    const canvas = document.querySelector('#pixelCanvas');
+
+    while (canvas.firstElementChild) {
+        canvas.removeChild(canvas.firstElementChild);
+    }
+    
+    //create table rows and cells and append to canvas
+    for (let i = 0; i < gridHeight.value; i++) {
+        let row = document.createElement('tr');
+
+        for(let x = 0; x < gridWidth.value; x++) {
+            let cell = document.createElement('td');
+            row.appendChild(cell);
+        }
+        canvas.appendChild(row);
+    }
+  }
 
 const sizeForm = document.querySelector('#sizePicker');
 
 sizeForm.addEventListener('submit',makeGrid);
 
 
-function makeGrid(event) {
-  event.preventDefault();
-  console.log('The form has been submitted');
-  
-}
+
